e558a71a6555dfa6140e8601931a4c62332d9b16
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-03-04 17:43:56 +0800

    1

--- frontend/app/src/main/java/love/moonc/sparklink/data/remote/model/response/LoginResponse.kt
+++ frontend/app/src/main/java/love/moonc/sparklink/data/remote/model/response/LoginResponse.kt
@@ -4,9 +4,6 @@ import com.google.gson.annotations.SerializedName
 import love.moonc.sparklink.data.remote.model.entity.User
 
 data class LoginResponse(
-    @SerializedName("token")
-    val token: String,
-
-    @SerializedName("user")
-    val user: User
+    @SerializedName("token") val token: String,
+    @SerializedName("user") val user: User
 )
