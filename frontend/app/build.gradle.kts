76d6fc267d5275828e84fb5273b0a792e87ebc33
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-03-24 11:34:03 +0800

    1

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
     }
     buildTypes {
         release {
